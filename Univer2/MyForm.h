b6737ecf46349120466e8a137481b8ac7cfc9b5f
Alisher <alisher.sadykov.95@mail.ru>
2022-04-10 21:26:02 +0500

    Добавил форму

--- Univer2/MyForm.h
+++ Univer2/MyForm.h
@@ -34,6 +34,49 @@ namespace Univer2 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::DataGridView^ dataGridView2;
+	protected:
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ dataGridViewTextBoxColumn1;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column2;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column3;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column4;
+	private: System::Windows::Forms::DataGridViewCheckBoxColumn^ Column5;
+	private: System::Windows::Forms::DataGridViewCheckBoxColumn^ Column6;
+	private: System::Windows::Forms::DataGridViewCheckBoxColumn^ Column7;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column8;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column9;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button1;
+
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+
+
+
+	private: System::Windows::Forms::TextBox^ textBox8;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Label^ label7;
+	private: System::Windows::Forms::CheckBox^ checkBox1;
+	private: System::Windows::Forms::CheckBox^ checkBox2;
+	private: System::Windows::Forms::CheckBox^ checkBox3;
+	private: System::Windows::Forms::RichTextBox^ richTextBox1;
+	private: System::Windows::Forms::Label^ label8;
+	private: System::Windows::Forms::DataGridViewComboBoxColumn^ Column1;
+	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::Label^ label9;
+
+
+
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +91,387 @@ namespace Univer2 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->dataGridView2 = (gcnew System::Windows::Forms::DataGridView());
+			this->dataGridViewTextBoxColumn1 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column2 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column3 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column4 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column5 = (gcnew System::Windows::Forms::DataGridViewCheckBoxColumn());
+			this->Column6 = (gcnew System::Windows::Forms::DataGridViewCheckBoxColumn());
+			this->Column7 = (gcnew System::Windows::Forms::DataGridViewCheckBoxColumn());
+			this->Column8 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column9 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox8 = (gcnew System::Windows::Forms::TextBox());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox2 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox3 = (gcnew System::Windows::Forms::CheckBox());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->label8 = (gcnew System::Windows::Forms::Label());
+			this->Column1 = (gcnew System::Windows::Forms::DataGridViewComboBoxColumn());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->label9 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// dataGridView2
+			// 
+			this->dataGridView2->BackgroundColor = System::Drawing::SystemColors::GradientActiveCaption;
+			this->dataGridView2->CellBorderStyle = System::Windows::Forms::DataGridViewCellBorderStyle::Raised;
+			this->dataGridView2->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView2->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(10) {
+				this->dataGridViewTextBoxColumn1,
+					this->Column2, this->Column3, this->Column4, this->Column5, this->Column6, this->Column7, this->Column8, this->Column9, this->Column1
+			});
+			this->dataGridView2->GridColor = System::Drawing::SystemColors::MenuHighlight;
+			this->dataGridView2->Location = System::Drawing::Point(6, 21);
+			this->dataGridView2->Name = L"dataGridView2";
+			this->dataGridView2->Size = System::Drawing::Size(1044, 357);
+			this->dataGridView2->TabIndex = 1;
+			this->dataGridView2->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MyForm::dataGridView2_CellContentClick);
+			// 
+			// dataGridViewTextBoxColumn1
+			// 
+			this->dataGridViewTextBoxColumn1->HeaderText = L"ID";
+			this->dataGridViewTextBoxColumn1->Name = L"dataGridViewTextBoxColumn1";
+			this->dataGridViewTextBoxColumn1->Resizable = System::Windows::Forms::DataGridViewTriState::True;
+			// 
+			// Column2
+			// 
+			this->Column2->HeaderText = L"ФИО";
+			this->Column2->Name = L"Column2";
+			// 
+			// Column3
+			// 
+			this->Column3->HeaderText = L"Номер телефона";
+			this->Column3->Name = L"Column3";
+			this->Column3->Resizable = System::Windows::Forms::DataGridViewTriState::True;
+			this->Column3->SortMode = System::Windows::Forms::DataGridViewColumnSortMode::NotSortable;
+			// 
+			// Column4
+			// 
+			this->Column4->HeaderText = L"Эл почта";
+			this->Column4->Name = L"Column4";
+			this->Column4->Resizable = System::Windows::Forms::DataGridViewTriState::True;
+			this->Column4->SortMode = System::Windows::Forms::DataGridViewColumnSortMode::NotSortable;
+			// 
+			// Column5
+			// 
+			this->Column5->HeaderText = L"ДопИнтерьер";
+			this->Column5->Name = L"Column5";
+			// 
+			// Column6
+			// 
+			this->Column6->HeaderText = L"4к";
+			this->Column6->Name = L"Column6";
+			this->Column6->Resizable = System::Windows::Forms::DataGridViewTriState::True;
+			this->Column6->SortMode = System::Windows::Forms::DataGridViewColumnSortMode::Automatic;
+			// 
+			// Column7
+			// 
+			this->Column7->HeaderText = L"Съемка с воздуха";
+			this->Column7->Name = L"Column7";
+			// 
+			// Column8
+			// 
+			this->Column8->HeaderText = L"Идея съемки";
+			this->Column8->Name = L"Column8";
+			this->Column8->Resizable = System::Windows::Forms::DataGridViewTriState::True;
+			this->Column8->SortMode = System::Windows::Forms::DataGridViewColumnSortMode::NotSortable;
+			// 
+			// Column9
+			// 
+			this->Column9->HeaderText = L"Хронометраж";
+			this->Column9->Name = L"Column9";
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Viner Hand ITC", 9.75F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->button2->Location = System::Drawing::Point(1119, 21);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(213, 51);
+			this->button2->TabIndex = 5;
+			this->button2->Text = L"Удалить";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Font = (gcnew System::Drawing::Font(L"Viner Hand ITC", 9.75F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->button4->Location = System::Drawing::Point(1119, 178);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(213, 51);
+			this->button4->TabIndex = 7;
+			this->button4->Text = L"Обновить";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Font = (gcnew System::Drawing::Font(L"Viner Hand ITC", 9.75F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->button3->Location = System::Drawing::Point(1119, 100);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(213, 51);
+			this->button3->TabIndex = 6;
+			this->button3->Text = L"Добавить";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Viner Hand ITC", 9.75F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(1119, 252);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(213, 51);
+			this->button1->TabIndex = 4;
+			this->button1->Text = L"Загрузить";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(359, 461);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(187, 20);
+			this->textBox2->TabIndex = 9;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(359, 414);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(187, 20);
+			this->textBox3->TabIndex = 10;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(687, 606);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(187, 20);
+			this->textBox4->TabIndex = 11;
+			// 
+			// textBox8
+			// 
+			this->textBox8->Location = System::Drawing::Point(359, 503);
+			this->textBox8->Name = L"textBox8";
+			this->textBox8->Size = System::Drawing::Size(187, 20);
+			this->textBox8->TabIndex = 15;
+			// 
+			// button5
+			// 
+			this->button5->Font = (gcnew System::Drawing::Font(L"Viner Hand ITC", 9.75F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->button5->Location = System::Drawing::Point(1119, 327);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(213, 51);
+			this->button5->TabIndex = 16;
+			this->button5->Text = L"Excel";
+			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Click += gcnew System::EventHandler(this, &MyForm::button5_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(270, 414);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(34, 13);
+			this->label1->TabIndex = 17;
+			this->label1->Text = L"ФИО";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(581, 468);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(75, 13);
+			this->label2->TabIndex = 18;
+			this->label2->Text = L"Идея съемки";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(581, 421);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(98, 13);
+			this->label3->TabIndex = 19;
+			this->label3->Text = L"Съемка с вохдуха";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(301, 613);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(19, 13);
+			this->label4->TabIndex = 20;
+			this->label4->Text = L"4k";
+			this->label4->Click += gcnew System::EventHandler(this, &MyForm::label4_Click);
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(248, 563);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(80, 13);
+			this->label5->TabIndex = 21;
+			this->label5->Text = L"Доп Интерьер";
+			this->label5->Click += gcnew System::EventHandler(this, &MyForm::label5_Click);
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(248, 503);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(56, 13);
+			this->label6->TabIndex = 22;
+			this->label6->Text = L"Эл. Почта";
+			this->label6->Click += gcnew System::EventHandler(this, &MyForm::label6_Click);
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(227, 461);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(93, 13);
+			this->label7->TabIndex = 23;
+			this->label7->Text = L"Номер телефона";
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(359, 563);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(15, 14);
+			this->checkBox1->TabIndex = 24;
+			this->checkBox1->UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this->checkBox2->AutoSize = true;
+			this->checkBox2->Location = System::Drawing::Point(701, 421);
+			this->checkBox2->Name = L"checkBox2";
+			this->checkBox2->Size = System::Drawing::Size(15, 14);
+			this->checkBox2->TabIndex = 25;
+			this->checkBox2->UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this->checkBox3->AutoSize = true;
+			this->checkBox3->Location = System::Drawing::Point(359, 613);
+			this->checkBox3->Name = L"checkBox3";
+			this->checkBox3->Size = System::Drawing::Size(15, 14);
+			this->checkBox3->TabIndex = 26;
+			this->checkBox3->UseVisualStyleBackColor = true;
+			this->checkBox3->CheckedChanged += gcnew System::EventHandler(this, &MyForm::checkBox3_CheckedChanged);
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Location = System::Drawing::Point(687, 468);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(198, 81);
+			this->richTextBox1->TabIndex = 27;
+			this->richTextBox1->Text = L"";
+			// 
+			// label8
+			// 
+			this->label8->AutoSize = true;
+			this->label8->Location = System::Drawing::Point(579, 613);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(77, 13);
+			this->label8->TabIndex = 28;
+			this->label8->Text = L"Хронометраж";
+			// 
+			// Column1
+			// 
+			this->Column1->HeaderText = L"Формат";
+			this->Column1->Name = L"Column1";
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Location = System::Drawing::Point(687, 563);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(121, 21);
+			this->comboBox1->TabIndex = 29;
+			// 
+			// label9
+			// 
+			this->label9->AutoSize = true;
+			this->label9->Location = System::Drawing::Point(579, 566);
+			this->label9->Name = L"label9";
+			this->label9->Size = System::Drawing::Size(49, 13);
+			this->label9->TabIndex = 30;
+			this->label9->Text = L"Формат";
+			this->label9->Click += gcnew System::EventHandler(this, &MyForm::label9_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1366, 720);
+			this->Controls->Add(this->label9);
+			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->label8);
+			this->Controls->Add(this->richTextBox1);
+			this->Controls->Add(this->checkBox3);
+			this->Controls->Add(this->checkBox2);
+			this->Controls->Add(this->checkBox1);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->textBox8);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->dataGridView2);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void dataGridView1_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+	}
+
+private: System::Void button5_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label6_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label5_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label4_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void checkBox3_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void dataGridView2_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+}
+private: System::Void label9_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
